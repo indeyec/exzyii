13a0891561db2d508bff9787037833cff2f5830f
Vladislav Shilov <vlad.shilov03@mail.ru>
2023-09-21 12:27:46 +0700

    first commit

---
+++ basic/models/Request.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "request".
+ *
+ * @property int $id
+ * @property int $id_user
+ * @property int $id_category
+ * @property string $name
+ * @property string $description
+ * @property string $image
+ * @property string $date
+ *
+ * @property Category $category
+ * @property User $user
+ */
+class Request extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'request';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id_user', 'id_category', 'name', 'description', 'image'], 'required'],
+            [['id_user', 'id_category'], 'integer'],
+            [['description'], 'string'],
+            [['date'], 'safe'],
+            [['name', 'image'], 'string', 'max' => 255],
+            [['id_category'], 'exist', 'skipOnError' => true, 'targetClass' => Category::class, 'targetAttribute' => ['id_category' => 'id']],
+            [['id_user'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['id_user' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'id_user' => 'Id User',
+            'id_category' => 'Id Category',
+            'name' => 'Name',
+            'description' => 'Description',
+            'image' => 'Image',
+            'date' => 'Date',
+        ];
+    }
+
+    /**
+     * Gets query for [[Category]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Category::class, ['id' => 'id_category']);
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::class, ['id' => 'id_user']);
+    }
+}
